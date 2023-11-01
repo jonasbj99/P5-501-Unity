1454cbcb50d18fa51a5a0d60913b99b608617e97
Kristoffer Troelsen <kristroelsen@hotmail.com>
2023-11-01 13:46:07 +0100

    s

---
+++ Assets/Scripts/TovTræk/TovPosition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TovPosition : MonoBehaviour
+{
+    public GameObject Tov;
+    private float YPos = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
