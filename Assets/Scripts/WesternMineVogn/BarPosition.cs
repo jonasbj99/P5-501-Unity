863e2e8cfa7e99476f14fde9cdfde54b3847a3a3
Kristoffer Troelsen <kristroelsen@hotmail.com>
2023-11-08 13:50:01 +0100

    western

--- Assets/Scripts/WesternMineVogn/BarPosition.cs
+++ Assets/Scripts/WesternMineVogn/BarPosition.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BarPosition : MonoBehaviour
 {
+    public GameObject AttachPole;
+    private Vector3 pos;
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class BarPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        offset = new Vector3(0, 0.3f, 0.2f);
+
+        pos = AttachPole.transform.position;
+
+        if(transform.position.z != pos.z)
+        {
+            transform.position = pos + offset;
+        }
     }
 }
--- Assets/Scripts/WesternMineVogn/PlayerPosition.cs
+++ Assets/Scripts/WesternMineVogn/PlayerPosition.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerPosition : MonoBehaviour
 {
+    public GameObject AttachPole;
+    private Vector3 pos;
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class PlayerPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        offset = new Vector3(0, -0.9f, 1.1f);
+
+        pos = AttachPole.transform.position;
+
+        if(transform.position != pos)
+        {
+            transform.position = pos + offset;
+        }
     }
 }
