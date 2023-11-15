b0b3de320f84b00787f01f4a111717c607274b09
Kristoffer Troelsen <kristroelsen@hotmail.com>
2023-11-15 23:05:41 +0100

    w

--- Assets/Scripts/TovTræk/PointCounter.cs
+++ Assets/Scripts/TovTræk/PointCounter.cs
@@ -5,18 +5,7 @@ using UnityEngine;
 public class PointCounter : MonoBehaviour
 {
     private int score = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnTriggerEnter (Collider other)
     {
         if(other.CompareTag("TovTrækPointCounter"))
