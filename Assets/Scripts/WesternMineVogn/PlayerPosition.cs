d4f0504e72cbbe3db05a4ace27e7f8a4e1bb7320
Kristoffer Troelsen <kristroelsen@hotmail.com>
2023-11-17 13:48:41 +0100

    waa

--- Assets/Scripts/WesternMineVogn/PlayerPosition.cs
+++ Assets/Scripts/WesternMineVogn/PlayerPosition.cs
@@ -7,16 +7,18 @@ public class PlayerPosition : MonoBehaviour
     public GameObject AttachPole;
     private Vector3 pos;
     private Vector3 offset;
+    private Vector3 initialOffset = new Vector3(0.1f, -1.1f, 0.7f);
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 initialPosition = AttachPole.transform.position + initialOffset;
+        transform.position = initialPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        offset = new Vector3(0, -0.9f, 1.1f);
+        offset = new Vector3(0.1f, -1.1f, 0.7f);
 
         pos = AttachPole.transform.position;
 
