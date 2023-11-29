52ab6d560358c1bf4c319a4ca7de979ac9ae95a1
Kristoffer Troelsen <kristroelsen@hotmail.com>
2023-11-29 14:27:03 +0100

    point time

--- Assets/Scripts/TovTræk/PointCounter.cs
+++ Assets/Scripts/TovTræk/PointCounter.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PointCounter : MonoBehaviour
 {
     private int score = 0;
+
+    public static float currentTime = 10f;
+    [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text timeText;
+
+
+    void Update()
+    {
+        if(currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            currentTime = 0;
+        }
+
+        scoreText.text = "Score: " + score;
+        timeText.text = "Time: " + currentTime.ToString("0");
+
+    }
     
     private void OnTriggerEnter (Collider other)
     {
