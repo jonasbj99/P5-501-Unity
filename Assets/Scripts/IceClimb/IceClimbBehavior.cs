7b28995770bb5447a38a08d095683f2e979b55b3
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2023-11-30 19:32:17 +0100

    HjemIs
    Mangler skilt til tid, mangler particle effect ved finish, mangler particle effect ved is hak, mangler test af lyd og mangler test af timer.

---
+++ Assets/Scripts/IceClimb/IceClimbBehavior.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class IceClimbBehavior : MonoBehaviour
+{
+    [SerializeField] GameObject signObject;
+    [SerializeField] TMP_Text climbTimeText;
+    public static float climbTime = 0f;
+    bool climbFinish;
+    bool climbStart;
+
+    void Update()
+    {
+        if (climbStart == true && climbFinish == false)
+        {
+            climbTime += Time.deltaTime;
+        }
+        else if (climbStart == true && climbFinish == true)
+        {
+            climbTimeText.text = "Time: " + climbTime.ToString("0.00");
+            signObject.gameObject.SetActive(true);
+            climbStart = false;
+        }
+    }
+
+    public void ClimbStart()
+    {
+        climbStart = true;
+    }
+
+    public void ClimbFinish()
+    {
+        climbFinish = true;
+    }
+}
